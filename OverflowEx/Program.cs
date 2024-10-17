41dd1bfc90764d720a2f786d70fe351f5153e6f0
ArchanaHarikrishnan <archana18harikrishnan@gmail.com>
2024-10-17 13:20:04 +0530

    Add project files.

---
+++ OverflowEx/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace OverflowEx
+{
+    public class OverflowEx
+    {
+        public static void Main(String[] args)
+        {
+
+            try
+            {
+
+                int num1 = int.MaxValue;
+                int num2 = int.MaxValue;
+
+                checked
+                {
+
+                    int mul = num1 * num2;
+                    Console.WriteLine("Multiplication: " + mul);
+                }
+
+            }
+            catch (OverflowException ex)
+            {
+
+                Console.WriteLine("You stored larger data in small datatype");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
